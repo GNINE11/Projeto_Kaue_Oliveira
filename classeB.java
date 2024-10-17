0d6dd731d44b019b97060d50985aa8cf6facdafd
kaue-oliveira <kaueolivsil@gmail.com>
2024-10-17 20:06:20 -0300

    Adicionado método MB3() na Classe B

--- classeB.java
+++ classeB.java
@@ -2,30 +2,33 @@ package Projeto_Kaue_Oliveira;
 
 public class classeB {
   private int B1;
-  private float B2;
+    private float B2;
 
-  public void setB1(int B1){
-    this.B1 = B1;
-  }
+    public int getB1() {
+        return B1;
+    }
 
-  public void setB2(float B2){
-    this.B2 = B2;
-  }
+    public void setB1(int b1) {
+        B1 = b1;
+    }
 
-  public int getB1(){
-    return B1;
-  }
-  public float getB2(){
-    return B2;
-  }
+    public float getB2() {
+        return B2;
+    }
 
+    public void setB2(float b2) {
+        B2 = b2;
+    }
 
-  public void MB1(){
-    System.out.println("O nome do metodo é MA1");
-  }
+    public void MB1() {
+        System.out.println("MB1");
+    }
 
+    public void MB2() {
+        System.out.println("MB2");
+    }
 
-  public void MB2(){
-    System.out.println("O nome do metodo é MA2");
-  }
+    public void MB3() {
+        System.out.println("MB3");
+    }
 }
